10cc9b06915af27483ebc4db155a44d894155c03
Iqbal <iqbal.sukur@mavs.uta.edu>
2018-10-07 04:22:45 -0500

    added config file

---
+++ Config/Config.js
@@ -0,0 +1,12 @@
+//Initializing Firebase
+
+export const DB_CONFIG = {
+
+    apiKey: "AIzaSyDxjzS4NZo_eiGTrtmVaKG8cHp1B3311T4",
+    authDomain: "extreme-flame-140021.firebaseapp.com",
+    databaseURL: "https://extreme-flame-140021.firebaseio.com",
+    projectId: "extreme-flame-140021",
+    storageBucket: "extreme-flame-140021.appspot.com",
+    messagingSenderId: "410924153604"
+      
+}
