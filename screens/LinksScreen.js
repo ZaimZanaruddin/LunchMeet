79c06576e7a9db5f2d816d1e5a626e5491e71d1d
Gilliam <gilliamjv@pd-124275.local>
2018-10-07 03:33:28 -0500

    Working yelp api listing

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import Touchable from 'react-native-platform-touchable';
 
 export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Browse',
-  };
 
   state = {
     data: []
@@ -16,22 +13,39 @@ export default class LinksScreen extends React.Component {
   }
 
   fetchData = async () => {
-    const response = await fetch('https://randomuser.me/api?results=10');
+    const response = await fetch('https://api.yelp.com/v3/businesses/search?location=Arlington', {
+                                    method: 'GET',
+                                    headers: { 'Authorization': 'Bearer vA2lOn3UepbvPt3y59mrzpLFqzqFBzNHWeZ3sZQXu8yIg1LNAHxmQjes10VUheVr8IeCBcvNyx6rSnYPKOmLVzU6vtO_9WAHwWPTsp-vNjFFfFH9Au6rN2Vm2Yq5W3Yx' }
+                                  }
+                                )
     const json = await response.json();
-    this.setState({ data: json.results });
+    console.log(json.businesses[0].name);
+    this.setState({ data: json.businesses});
   }
 
 
   render() {
     return (
       <View style={styles.container}>
+      
         <FlatList data={this.state.data}
         keyExtractor={(x, i) => i.toString()}
         renderItem={
           ({ item }) => 
-            <Text>
-              {item.name.first} {item.name.last}
-            </Text>
+            <Touchable
+            onPress={() => console.log('hello')}
+            style={{
+              backgroundColor: '#e5e5e5',
+              marginTop: 5,
+              paddingVertical: 30,
+              paddingHorizontal: 80,
+              alignItems: 'center'
+            }}
+            >
+              <Text>
+                {item.name}
+              </Text>
+            </Touchable>
           }
         />
       </View>
